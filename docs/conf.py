885b60743cda711056892cc6806ec1921237d070
yangjucai <1165456797@qq.com>
2025-03-14 19:33:19 +0800

    delete all .mo files under zh_CN; add autoclass_content in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ sys.path.insert(0, os.path.abspath('../bazel-bin/heu/pybind'))
 project = 'HEU'
 copyright = '2022-2024 Ant Group Co., Ltd'
 author = 'HEU authors'
+autoclass_content='both'
 
 # The full version, including alpha/beta/rc tags
 # release = '0.0.6'
